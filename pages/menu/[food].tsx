d3508ae226676b56784086e6060ae94c684f96fd
Rohollah <rohollah.malekzadeh@gmail.com>
2022-09-08 23:20:08 +0200

    Added filter Ui feature to food Page

--- pages/menu/[food].tsx
+++ pages/menu/[food].tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, ChangeEvent} from 'react'
 import {GetStaticProps, GetStaticPaths} from 'next'
 
 import Row from 'src/components/ui/row'
@@ -6,6 +6,8 @@ import OrderPoster from 'src/components/poster/orderPoster'
 import RowsContainer from 'src/components/ui/rowsContainer'
 import AddToCartButton from 'src/components/addToCartButton/addToCartButton'
 import Price from 'src/components/price/price'
+import StickyLayout from 'src/components/ui/stickyLayout'
+import DropdownOnClick from 'src/components/ui/dropDownOnClick'
 
 import {getFoodData} from 'src/config-api/food-api/order-list'
 import {ORDER} from 'src/link'
@@ -16,9 +18,22 @@ import {
   DIET_QUERY,
   FOOD_QUERY,
   MEAL_QUERY,
+  cuisineType,
 } from 'src/config-api/food-api/config'
+import CheckboxInput from 'src/components/ui/checkboxInput'
 
 import {CartItems} from 'types/data'
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = ORDER.map((item) => {
+    return {params: {food: Object.values(item).join('')}}
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
 
 export const getStaticProps: GetStaticProps = async (context) => {
   let data = {} as CartItems
@@ -34,44 +49,100 @@ export const getStaticProps: GetStaticProps = async (context) => {
     data = await getFoodData(foodType, FOOD_LIST, {sliceFrom: 0, sliceTo: 8})
   }
 
+  const list =
+    foodType === 'diet'
+      ? DIET_LIST
+      : foodType === 'mealType'
+      ? MEAL_LIST
+      : foodType === 'q'
+      ? FOOD_LIST
+      : ''
+  const defaultObject = {} as any
+  Object.values(list).forEach((item) => (defaultObject[item] = false))
+
   //*Don't change revalidate time, because all prices are fake
-  //*Don't change rendering page to CSR, because all prices are fake
+  //*Or
+  //*Don't change rendering type to CSR, because  there is a limit when calling api:10 times per min
   return {
     props: {
       data,
+      defaultObject,
     },
     revalidate: 2000000,
   }
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = ORDER.map((item) => {
-    return {params: {food: Object.values(item).join('')}}
-  })
+const Food = ({data, defaultObject}: any) => {
+  const [filter, setFilter] = useState({...defaultObject, ...cuisineType})
 
-  return {
-    paths,
-    fallback: false,
+  const handleCheck = (e: ChangeEvent<HTMLInputElement>) => {
+    const {checked, value} = e.target
+    setFilter({...filter, [value]: checked})
   }
-}
 
-const Food = ({data}: any) => {
+  //*This is a beginning idea
+  //*I'm going to change it, add some variables from react query and make it clean.
+  //TODO:Change DropdownOnClick to Dropdown
+
   return (
-    <div>
-      {data.map((item: any) => (
-        <RowsContainer key={item.title}>
-          <Row title={item.title}>
-            {item.data.map((item: any) => (
-              <OrderPoster
-                key={item.label}
-                {...item}
-                addToCartComponent={<AddToCartButton item={item} />}
-                priceComponent={<Price price={item.price} off={item.off} />}
+    <div className="flex justify-center items-start gap-14 relative">
+      <div>
+        {data.map((item: any) => (
+          <RowsContainer key={item.title}>
+            <Row title={item.title}>
+              {item.data.map((item: any) => (
+                <OrderPoster
+                  key={item.label}
+                  {...item}
+                  addToCartComponent={<AddToCartButton item={item} />}
+                  priceComponent={<Price price={item.price} off={item.off} />}
+                />
+              ))}
+            </Row>
+          </RowsContainer>
+        ))}
+      </div>
+
+      <StickyLayout>
+        <h6 className="text-4xl border-b-2 pb-1 border-yellow-300 mb-2">
+          Filter
+        </h6>
+        <DropdownOnClick
+          title="Meal"
+          className="flex flex-col p-1 border-b-2 border-yellow-300 text-2xl"
+        >
+          <div className="bg-white px-4  text-lg">
+            {Object.keys(defaultObject).map((item) => (
+              <CheckboxInput
+                label={item}
+                value={item}
+                name={item}
+                onChange={handleCheck}
+                checked={filter.item}
+                key={item}
               />
             ))}
-          </Row>
-        </RowsContainer>
-      ))}
+          </div>
+        </DropdownOnClick>
+
+        <DropdownOnClick
+          title="Cuisine Type"
+          className="flex flex-col p-1 border-b-2 border-yellow-300 text-2xl"
+        >
+          <div className="bg-white px-4 text-lg">
+            {Object.keys(cuisineType).map((item) => (
+              <CheckboxInput
+                label={item}
+                value={item}
+                name={item}
+                onChange={handleCheck}
+                checked={filter.item}
+                key={item}
+              />
+            ))}
+          </div>
+        </DropdownOnClick>
+      </StickyLayout>
     </div>
   )
 }
